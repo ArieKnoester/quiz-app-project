e0305e1f21b0d0f835241316d2d7442edce7753f
Arie Knoester <arikstr@gmail.com>
2023-11-14 11:42:43 -0600

    Added UI elements.

--- ui.py
+++ ui.py
@@ -1,2 +1,31 @@
+import tkinter as tk
 THEME_COLOR = "#375362"
+SCORE_LABEL_FONT = ("Ariel", 14, "normal")
+QUESTION_FONT = ("Ariel", 20, "italic")
 
+
+class QuizUi():
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Quizler")
+        self.window.config(background=THEME_COLOR, padx=20, pady=20)
+        self.score = 0
+
+        # Question canvas
+        self.canvas = tk.Canvas(width=300, height=250, background="white")
+        self.canvas.config()
+        self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
+        self.question_text = self.canvas.create_text(150, 125, text="STUFF", font=QUESTION_FONT)
+
+        # Labels
+        self.score_label = tk.Label(text=f"Score: {self.score}", font=SCORE_LABEL_FONT, foreground="white", background=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        # Buttons
+        self.true_button_img = tk.PhotoImage(file="images/true.png")
+        self.true_button = tk.Button(image=self.true_button_img, padx=20, pady=20, borderwidth=0)
+        self.true_button.grid(row=2, column=0, padx=20, pady=20)
+        self.false_button_img = tk.PhotoImage(file="images/false.png")
+        self.false_button = tk.Button(image=self.false_button_img, padx=20, pady=20, borderwidth=0)
+        self.false_button.grid(row=2, column=1, padx=20, pady=20)
+        self.window.mainloop()
