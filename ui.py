4fd37674bea4ac40eb2e085441ea970fdad704a0
Arie Knoester <arikstr@gmail.com>
2023-11-14 14:57:43 -0600

    Added user feedback and scoring.

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from quiz_brain import QuizBrain
 
+
 THEME_COLOR = "#375362"
 SCORE_LABEL_FONT = ("Ariel", 14, "normal")
 QUESTION_FONT = ("Ariel", 20, "italic")
@@ -16,12 +17,11 @@ class QuizUi:
 
         # Question canvas
         self.canvas = tk.Canvas(width=300, height=250, background="white")
-        self.canvas.config()
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
         self.question_text = self.canvas.create_text(150, 125, text="", font=QUESTION_FONT, width=280)
 
         # Labels
-        self.score = self.score_label = tk.Label(
+        self.score_label = tk.Label(
             text=f"Score: {self.quiz.score}",
             font=SCORE_LABEL_FONT,
             foreground="white",
@@ -56,7 +56,27 @@ class QuizUi:
         self.canvas.itemconfig(self.question_text, text=next_question_text)
 
     def user_answers_true(self):
-        self.quiz.check_answer(user_answer="True")
+        correct = self.quiz.check_answer(user_answer="True")
+        self.display_if_correct(correct)
 
     def user_answers_false(self):
-        self.quiz.check_answer(user_answer="False")
+        correct = self.quiz.check_answer(user_answer="False")
+        self.display_if_correct(correct)
+
+    def display_if_correct(self, correct):
+        if correct:
+            self.canvas.config(background="green")
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.config(background="red")
+
+        self.canvas.after(1000, self.display_next)
+
+    def display_next(self):
+        self.canvas.config(background="white")
+        if self.quiz.still_has_questions():
+            self.get_next_question()
+        else:
+            self.canvas.itemconfig(self.question_text, text="You have completed the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
