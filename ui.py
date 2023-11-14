2f232dbd636ba41ebbe17d4199888d87b37a3ef4
Arie Knoester <arikstr@gmail.com>
2023-11-14 13:19:02 -0600

    Added retrieval of first quiz question.

--- ui.py
+++ ui.py
@@ -1,11 +1,15 @@
 import tkinter as tk
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 SCORE_LABEL_FONT = ("Ariel", 14, "normal")
 QUESTION_FONT = ("Ariel", 20, "italic")
 
 
-class QuizUi():
-    def __init__(self):
+class QuizUi:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = tk.Tk()
         self.window.title("Quizler")
         self.window.config(background=THEME_COLOR, padx=20, pady=20)
@@ -15,10 +19,15 @@ class QuizUi():
         self.canvas = tk.Canvas(width=300, height=250, background="white")
         self.canvas.config()
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
-        self.question_text = self.canvas.create_text(150, 125, text="STUFF", font=QUESTION_FONT)
+        self.question_text = self.canvas.create_text(150, 125, text="", font=QUESTION_FONT, width=280)
 
         # Labels
-        self.score_label = tk.Label(text=f"Score: {self.score}", font=SCORE_LABEL_FONT, foreground="white", background=THEME_COLOR)
+        self.score_label = tk.Label(
+            text=f"Score: {self.score}",
+            font=SCORE_LABEL_FONT,
+            foreground="white",
+            background=THEME_COLOR
+        )
         self.score_label.grid(row=0, column=1)
 
         # Buttons
@@ -28,4 +37,10 @@ class QuizUi():
         false_button_img = tk.PhotoImage(file="images/false.png")
         self.false_button = tk.Button(image=false_button_img, padx=20, pady=20, borderwidth=0)
         self.false_button.grid(row=2, column=1, padx=20, pady=20)
+
+        self.get_next_question()
         self.window.mainloop()
+
+    def get_next_question(self):
+        next_question_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=next_question_text)
